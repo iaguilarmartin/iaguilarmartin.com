c62e11d41ba485d1b63562a62a74ae8f33c5f7eb
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2019-01-02 12:04:59 +0100

    Setup client project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/js/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/client/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }],
+        include: [path.resolve('./src')]
+      }
+    ]
+  }
+};
